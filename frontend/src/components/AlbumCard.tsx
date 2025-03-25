12db1da53f4a3475135ca164e448a34739b54e0d
Abhishek-Bharti1 <abhishekbharti91550@gmail.com>
2025-03-25 09:11:51 +0530

    player added

---
+++ frontend/src/components/AlbumCard.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { useNavigate } from "react-router-dom"
+
+
+interface AlbumCardProps{
+    image:string,
+    name:string,
+    desc:string,
+    id:string
+}
+const AlbumCard:React.FC<AlbumCardProps> = ({image,name,desc,id}) => {
+    const navigate = useNavigate();
+  return (
+    <div className="mini-w-[180px] p-2 px-3 rounded cursor-pointer bg-[#ffffff26]"
+    onClick={()=>navigate('/album/'+id)}
+    >
+        <img src={image} className="rounded w-[160px]"/>
+        <p className="font-bold mt-2 mb-1">{name.slice(0,12)}...</p>
+        <p className="text-slate-200 text-sm">{desc.slice(0,18)}...</p>
+    </div>
+  )
+}
+
+export default AlbumCard;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,33 @@
-import Layout from "../components/layout"
+import AlbumCard from "../components/AlbumCard";
+import Layout from "../components/Layout"
+import SongCard from "../components/SongCard";
+import { useSongData } from "../context/SongContext"
 
 const Home = () => {
+  const {albums,songs} = useSongData();
   return (
   <div>
     <Layout>
-      home
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto gap-4">
+          {
+            albums?.map((e,i)=>(
+              <AlbumCard key={i} image={e.thumbnail} name={e.title} desc={e.description} id={e.id}/>
+            ))
+          }
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-auto gap-4">
+          {
+            songs?.map((e,i)=>(
+              <SongCard key={i} image={e.thumbnail} name={e.title} desc={e.description} id={e.id}/>
+            ))
+          }
+        </div>
+      </div>
     </Layout>
   </div>
   )
